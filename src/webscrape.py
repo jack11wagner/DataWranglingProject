2f8403f08f1c5575afa006bd29314ef90fc4832d
jack11wagner <jacksonjw519@gmail.com>
2021-10-29 22:54:12 -0400

    Loaded up past 5 years of Football Data
    * Changed getStatsFromScrape function for loading of csv directly from function

--- src/webscrape.py
+++ src/webscrape.py
@@ -3,40 +3,61 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# 2020 NFL Stats ONLY
-
-passing_url = 'https://www.pro-football-reference.com/years/2020/passing.htm'
-rushing_url = 'https://www.pro-football-reference.com/years/2020/rushing.htm'
-receiving_url = 'https://www.pro-football-reference.com/years/2020/receiving.htm'
-defense_url = 'https://www.pro-football-reference.com/years/2020/defense.htm'
+# 2016-2020 NFL Stats (Past 5 years)
 
 
-def getStatsFromScrape(category):
+# Only gets single DF at a time
+def oldGetStatsFromScrape(category):
     url = 'https://www.pro-football-reference.com/years/2020/{}.htm'.format(category)
     html = urlopen(url)
     soup = BeautifulSoup(html, features='html.parser')
     headers = [header.getText() for header in soup.findAll('tr', limit=2)[0].findAll('th')]
-
-    if headers[1] != 'Player':
-        headers = [header.getText() for header in soup.findAll('tr', limit=2)[1].findAll('th')]
-        rows = soup.findAll('tr')[2:]
-    else:
-        rows = soup.findAll('tr')[1:]
     headers = headers[1:]
-    
+    rows = soup.findAll('tr')[1:]
     player_stats = [[td.getText() for td in rows[i].findAll('td')]
                     for i in range(len(rows))]
     stats = pd.DataFrame(player_stats, columns=headers)
     return stats
 
 
+def getStatsFromScrape(year, category_list):
+    """
+
+    :param year: which year to search for in Football Reference
+    :param category_list:
+    :return:
+    """
+    for category in category_list:
+        url = 'https://www.pro-football-reference.com/years/{}/{}.htm'.format(year, category)
+        html = urlopen(url)
+        soup = BeautifulSoup(html, features='html.parser')
+        headers = [header.getText() for header in soup.findAll('tr', limit=2)[0].findAll('th')]
+
+        if headers[1] != 'Player':
+            headers = [header.getText() for header in soup.findAll('tr', limit=2)[1].findAll('th')]
+            rows = soup.findAll('tr')[2:]
+        else:
+            rows = soup.findAll('tr')[1:]
+        headers = headers[1:]
+
+        player_stats = [[td.getText() for td in rows[i].findAll('td')]
+                        for i in range(len(rows))]
+        stats = pd.DataFrame(player_stats, columns=headers)
+        # Added Dataframe Column for our reference when looking at csv files
+        stats['Year'] = year
+        stats['Category'] = category
+        stats.to_csv("stats/{}/{}-{}.csv".format(year, category, year), index=False, header=True)
+
+
 def main():
+    # years = [2016, 2017, 2018, 2019, 2020]
+    categories = ['passing', 'rushing', 'receiving', 'defense']
 
-    passing_df, receiving_df, rushing_df = getStatsFromScrape('passing'), getStatsFromScrape('receiving'), getStatsFromScrape('rushing')
-
-    passing_df.to_csv('stats/passing2020.csv', index=False)
-    receiving_df.to_csv('stats/receiving2020.csv', index=False)
-    rushing_df.to_csv('stats/rushing2020.csv', index=False)
+    getStatsFromScrape(2016, categories)
+    getStatsFromScrape(2017, categories)
+    getStatsFromScrape(2018, categories)
+    getStatsFromScrape(2019, categories)
+    getStatsFromScrape(2020, categories)
 
 
 if __name__ == "__main__":
