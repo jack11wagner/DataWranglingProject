9ce9c27eab7998061f91b999044fdec1f96dbef5
Tyler Valentine <valentinet02@moravian.edu>
2021-11-23 00:14:39 -0500

    Create working format for player stats

--- createBaseballDB.py
+++ createBaseballDB.py
@@ -1,7 +1,10 @@
 from dotenv import load_dotenv
 import mysql.connector, os, csv
-import webscrapeAwardsAndHonors, webscrapeCareerLeaders, webscrapeTopIndividualPerf
+import webscrapeHallOfFame, webscrapeCareerLeaders, webscrapeTopIndividualPerf
 from datetime import datetime
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
 
 
 def connect_to_SQL():
@@ -201,8 +204,90 @@ def getHallOfFamePlayers(filename, playerNameDictionary):
     return hall_of_fame_dictionary
 
 
-def getAllTimeLeadersDictionary(filedirectories, playerNameDictionary):
-    all_time_leaders = {}
+def getAllTimeLeadersDictionary(playerNameDictionary):
+    batters_list = []
+    pitchers_list = []
+    batters_headers = ['Player Name', 'G', 'AB', 'R', 'H', '2B', '3B', 'HR', 'RBI', 'BB', 'IBB', 'SO', 'HBP', 'SH', 'SF', 'XI', 'ROE',
+                       'GDP', 'SB', 'CS', 'AVG', 'OBP', 'SLG', 'BFW']
+
+    pitchers_headers = ['Player Name', 'G', 'GS', 'CG', 'SHO', 'GF', 'SV', 'IP', 'H', 'BFP', 'HR', 'R', 'ER', 'BB', 'IB', 'SO', 'SH',
+                        'SF', 'WP', 'HBP', 'BK', '2B', '3B', 'GDP', 'ROE', 'W', 'L', 'ERA', 'RS', 'PW']
+
+    for name in playerNameDictionary:
+        player_id = playerNameDictionary[name]
+        player_last_name_first_character = player_id[0].upper()
+
+        url = "https://www.retrosheet.org/boxesetc/" + player_last_name_first_character + "/P" + player_id + ".htm"
+        html = urlopen(url)
+        soup = BeautifulSoup(html, features='html.parser')
+        text = [words.getText() for words in soup.find_all('pre')]
+        text.remove(text[-1])
+        for line in text:
+            title = line.split("\n")[0].strip()
+            if title != "Fielding Record":
+                continue
+            position_text = line.split("\n")[2]
+            position = position_text[25:28].strip()
+
+            if position != "P":
+                batter_stats_list = []
+                batter_stats_list.append(name)
+                for line in text:
+                    title = line.split("\n")[0].strip()
+                    if title == "Batting Record":
+                        stats = line.split("\n")
+                        for stat in stats:
+                            categories = stat.split(" ")
+                            if categories[0] == "Total" and categories[1] == '':
+                                categories.remove(categories[0])
+                                categories.remove(categories[0])
+                                categories.remove(categories[0])
+                                categories.remove(categories[0])
+                                categories.remove(categories[0])
+                                categories.remove(categories[0])
+                                categories.remove(categories[0])
+                                for category in categories:
+                                    if category != '' and category != "Total" and category != "Splits" and category != ")":
+                                        batter_stats_list.append(category)
+                                batters_list.append(batter_stats_list)
+                                batter_stats_df = pd.DataFrame(batters_list, columns=batters_headers)
+                                print(batter_stats_df)
+                                break
+
+            else:
+                pitcher_stats_list = []
+                pitcher_stats_list.append(name)
+                for line in text:
+                    title = line.split("\n")[0].strip()
+                    if title == "Pitching Record":
+                        stats = line.split("\n")
+                        for stat in stats:
+                            categories = stat.split(" ")
+                            if categories[0] == "Total" and categories[1] == '':
+                                categories.remove(categories[0])
+                                categories.remove(categories[0])
+                                categories.remove(categories[0])
+                                categories.remove(categories[0])
+                                categories.remove(categories[0])
+                                categories.remove(categories[0])
+                                for category in categories:
+                                    if category == "-":
+                                        pitcher_stats_list.append("None")
+                                        continue
+                                    if category != '' and category != "Total" and category != "Splits" and category != "Years)":
+                                        pitcher_stats_list.append(category)
+                                pitchers_list.append(pitcher_stats_list)
+                                pitcher_stats_df = pd.DataFrame(pitchers_list, columns=pitchers_headers)
+                                print(pitcher_stats_df)
+                                break
+
+
+
+
+
+
+    
+    """
     for file in filedirectories:
         in_file = open(file)
         in_file.readline()
@@ -228,16 +313,19 @@ def getAllTimeLeadersDictionary(filedirectories, playerNameDictionary):
     #     print(len(all_time_leaders[key]))
 
     return all_time_leaders
-
+    """
 
 def main():
     # loadBaseballData()
+    """
     cursor, conn = connect_to_SQL()
     createBaseballDB(cursor, "baseballStats_db")
     name_fields, player_bio_fields, hall_of_fame_fields, all_time_batting, all_time_pitching = createDBFields()
     #
     createTable(cursor, name_fields, 'Player_Names')
+    """
     player_names_dict = getPlayerNamesDictionary('playerinformation/playerBios.csv')
+    """
     load_Player_NamesTable(cursor, player_names_dict, 'Player_Names')
 
     createTable(cursor, player_bio_fields, 'Player_Bios')
@@ -250,7 +338,9 @@ def main():
 
     createTable(cursor, all_time_batting, 'All_Time_Batting')
     all_time_batting_dirs = sorted(getDataDirectories('battingstats/careerleaders/'))
-    all_time_batting = getAllTimeLeadersDictionary(all_time_batting_dirs, player_names_dict)
+    """
+    all_time_batting = getAllTimeLeadersDictionary(player_names_dict)
+    """
     loadAllTimeBattingLeaders(cursor, all_time_batting, 'All_Time_Batting')
 
     all_time_pitching_dirs = sorted(getDataDirectories('pitchingstats/careerleaders/'))
@@ -259,7 +349,7 @@ def main():
     loadAllTimePitchingLeaders(cursor, all_time_pitching, 'All_Time_Pitching')
 
     conn.commit()
-
+    """
 
 if __name__ == '__main__':
     main()
